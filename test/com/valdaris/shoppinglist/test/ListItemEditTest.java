600874976879742cb81b4054c2c64367d2e2de88
Javier Estevez <valdaris@gmail.com>
2012-03-19 15:08:45 +0100

    Mock JUnit Test form List Item Edit

---
+++ test/com/valdaris/shoppinglist/test/ListItemEditTest.java
@@ -0,0 +1,18 @@
+package com.valdaris.shoppinglist.test;
+
+import junit.framework.TestSuite;
+
+import org.junit.Test;
+
+public class ListItemEditTest extends TestSuite {
+    
+    public void setUp() {
+        
+    }
+    
+    @Test
+    public void createListItemTest() {
+        //TODO test creating list items
+    }
+
+}
